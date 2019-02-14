f9ccfed3e1201a246e257a83dc02ac2792505371
Mark Hollands <markjhollands@gmail.com>
2019-02-14 23:22:06 +0000

    Add simple test to check that telnetd is not installed

--- controls/example.rb
+++ controls/example.rb
@@ -1,19 +1,8 @@
 # encoding: utf-8
 # copyright: 2018, The Authors
 
-title 'sample section'
+# Disallow insecure protocols by testing
 
-# you can also use plain tests
-describe file('/tmp') do
-  it { should be_directory }
-end
-
-# you add controls here
-control 'tmp-1.0' do                        # A unique ID for this control
-  impact 0.7                                # The criticality, if this control fails.
-  title 'Create /tmp directory'             # A human-readable title
-  desc 'An optional description...'
-  describe file('/tmp') do                  # The actual test
-    it { should be_directory }
-  end
+describe package('telnetd') do
+  it { should_not be_installed }
 end
