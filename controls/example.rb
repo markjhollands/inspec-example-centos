b51a84e882dcf3408a1d85a7598051400f19ec47
Mark Hollands <markjhollands@gmail.com>
2019-02-14 23:23:06 +0000

    Add some run  instructions to test file

--- controls/example.rb
+++ controls/example.rb
@@ -3,6 +3,9 @@
 
 # Disallow insecure protocols by testing
 
+# To run this remotely run a command such as:
+# inspec exec . -t ssh://centos@34.212.241.130 -i C:\Users\chef\.ssh\id_rsa
+
 describe package('telnetd') do
   it { should_not be_installed }
 end
